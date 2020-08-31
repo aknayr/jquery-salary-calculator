10d5cc6ac76e4e3c747a974c7ed18bd1fb03e475
Ajay Nayr <ajaynayr@gmail.com>
2020-08-31 07:14:38 -0500

    Completed base mode

--- scripts/salaryCalculator.js
+++ scripts/salaryCalculator.js
@@ -13,5 +13,30 @@ function readyNow(){
 
 function submitForm(){
   console.log('Button was clicked');
-  
-}
+  let newEmployee = {
+    firstName: $('#firstName').val(),
+    lastName: $('#lastName').val(),
+    idNumber: $('#idNumber').val(),
+    jobTitle: $('#jobTitle').val(),
+    salary: $('#salary').val();
+  } // end newEmployee object
+  $('#employeeTable').append('
+  <tr>
+    <td> $(newEmployee.firstName) </td>
+    <td> $(newEmployee.lastName) </td>
+    <td> $(newEmployee.idNumber) </td>
+    <td> $(newEmployee.jobTitle) </td>
+    <td> $(newEmployee.salary) </td>
+  </tr>
+  ');
+  //empty out input fields
+  $('#firstName').val('');
+  $('#lastName').val('');
+  $('#idNumber').val('');
+  $('#jobTitle').val('');
+  $('#salary').val('');
+
+  employees.push(newEmployee);
+  monthlyCosts.push(employees);
+  return newEmployee;
+} // end submitForm
