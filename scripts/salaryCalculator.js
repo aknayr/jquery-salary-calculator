dc4064601f741d6995a108cd9f8d72d5dc4479d9
Ajay Nayr <ajaynayr@gmail.com>
2020-08-31 07:26:19 -0500

    completed monthly costs function

--- scripts/salaryCalculator.js
+++ scripts/salaryCalculator.js
@@ -18,7 +18,7 @@ function submitForm(){
     lastName: $('#lastName').val(),
     idNumber: $('#idNumber').val(),
     jobTitle: $('#jobTitle').val(),
-    salary: $('#salary').val();
+    salary: $('#salary').val()
   } // end newEmployee object
   $('#employeeTable').append('
   <tr>
@@ -27,8 +27,8 @@ function submitForm(){
     <td> $(newEmployee.idNumber) </td>
     <td> $(newEmployee.jobTitle) </td>
     <td> $(newEmployee.salary) </td>
-  </tr>
-  ');
+  </tr>')
+
   //empty out input fields
   $('#firstName').val('');
   $('#lastName').val('');
@@ -40,3 +40,33 @@ function submitForm(){
   monthlyCosts.push(employees);
   return newEmployee;
 } // end submitForm
+
+function monthylCosts(employeeArr) {
+  console.log('in monthlyCosts');
+  let totalCost = 0;
+  for (let i=0; i < employeeArr.length; i++) {
+    totalCost = totalCost + Number(employees[i].salary / 12);
+  } // end for loop
+  $('#monthlyCostDisplay').text('Monthly costs are $${totalCost}')
+
+  if (totalCost > 20000) {
+    $('#monthlyCostDisplay').css('background-color', 'light red');
+  } else {
+    $('#monthlyCostDisplay').css('background-color', 'light green');
+  } // end if-else
+  return totalCost;
+} // end function monthlyCosts
+
+function deleteEmployee{
+  console.log('in deleteEmployee');
+  let searchID = $(this).closest('tr').find('.empId').text();
+  searchId = searchId.trim();
+  for(let i=0; i < employee.length; i++){
+    if (employees[i].idNumber === searchId) {
+      employees.splice(i, 1);
+    } // end if
+  } // for-loop
+  $(this).parent().parent().remove();
+  monthlyCosts(employees);
+  return;
+} // end function deleteEmployee
